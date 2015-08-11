d24900f6d72af7609cc6774ba455fe52fe7349b1
actuallymentor <mentor@palokaj.co>
2015-08-11 09:56:14 +0200

    Email sig emptied

---
+++ wprm_vars.php
@@ -0,0 +1,13 @@
+<?php 
+
+// defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+return array (
+	'sendyuser' => 'USER',
+	'sendypass' => 'PASS',
+	'listid' => 'LIST',
+	'from' => 'YOUREMAIL',
+	'thankyou' => 'THANKSURL',
+	'signature' => 'EMAILSIG',
+	);
+
+?>
