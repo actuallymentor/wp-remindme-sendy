f7bd9478215240729312b00aef189026a4734635
actuallymentor <mentor@palokaj.co>
2015-08-24 10:07:33 +0200

    Return instead of echo shortcode

--- functions/shortcode.php
+++ functions/shortcode.php
@@ -10,27 +10,30 @@ function wprm_remindme_func(){
 
 	//Form spam control and debug declaration
 	if(!session_id()) {
-        session_start();
-    }
+		session_start();
+	}
 	$wprm_form_token = md5(uniqid('auth', true));
 	$_SESSION['wprm_form_token'] = $wprm_form_token;
 	if ($_GET['debug'] == true) {
 		$wprm_debug .= '?debug=true';
 	}
-	?>
-	<form method="POST" action=<?php echo '"' . $actorurl . $wprm_debug . '"'; ?> >
-		<input type="text" name="toname" placeholder="Name" />
-		<input type="email" name="tomail" placeholder="Email" />
-		<input type="text" name="from" value=<?php echo '"' . $wprm_config['from'] .  '"'; ?> hidden>
-		<input type="text" name="list" value=<?php echo '"' . $wprm_config['listid'] .  '"'; ?> hidden>
-		<input type="text" name="forward" value=<?php echo '"' . $wprm_config['thankyou'] .  '"'; ?> hidden>
-		<input type="text" name="subject" value=<?php echo '"' . $subject .  '"'; ?> hidden>
-		<input type="text" name="url" value=<?php echo '"' . $wprm_url .  '"'; ?> hidden>
-		<input type="text" name="title" value=<?php echo '"' . $title .  '"'; ?> hidden>
-		<input type="text" name="wprm_form_token" value=<?php echo '"' . $wprm_form_token .  '"'; ?> hidden>
-		<input type="submit" name="submit" value="Save to mail">
-	</form>
-	<?php
+
+	$wprm_return = 	'<form id="wprm_form" method="POST" action="' . $actorurl . $wprm_debug . '">
+	<input type="text" name="toname" placeholder="Name" />
+	<input type="email" name="tomail" placeholder="Email" />
+	<input type="text" name="from" value="' . $wprm_config['from'] . '" hidden>
+	<input type="text" name="list" value="'. $wprm_config['listid'] .  '" hidden>
+	<input type="text" name="forward" value="' . $wprm_config['thankyou'] .  '" hidden>
+	<input type="text" name="subject" value="' . $subject .  '" hidden>
+	<input type="text" name="url" value="' . $wprm_url .  '" hidden>
+	<input type="text" name="title" value="' . $title .  '" hidden>
+	<input type="text" name="wprm_form_token" value="' . $wprm_form_token .  '" hidden>
+	<input type="submit" name="submit" value="Save to mail">
+</form>';
+
+
+return $wprm_return;
+
 }
 add_shortcode( 'wprm_remindme', 'wprm_remindme_func' );
 ?>
